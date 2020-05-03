fc6d04df533f2ad343790cb9b883a9f77d0623ad
muzi <42566386+muzi502@users.noreply.github.com>
2020-05-03 19:15:03 +0800

    Update merge_bilibili_client_flv.sh

--- merge_bilibili_client_flv.sh
+++ merge_bilibili_client_flv.sh
@@ -5,33 +5,40 @@
 # 将该脚放到 UWP 客户端下载缓存主目录下执行，安装 ffmpeg、jq
 
 set -xu
-root_dir=$(pwd)
-mp4_dir=${root_dir}/mp4
+download_dir=$(pwd)
+mp4_dir=${download_dir}/mp4
 mkdir -p ${mp4_dir}
 
-for dir in $(ls | sort -n | grep -E -v "\.|mp4")
+for video_dir in $(ls | sort -n | grep -E -v "\.|mp4")
 do
-  cd ${root_dir}/${dir}
+  cd ${download_dir}/${video_dir}
   up_name=$(jq ".Uploader" *.dvi | tr -d "[:punct:]\040\011\012\015")
   mkdir -p ${mp4_dir}/${up_name}
   for p_dir in $(ls | sort -n | grep -v "\.")
   do
-    cd ${root_dir}/${dir}/${p_dir}
+    cd ${download_dir}/${video_dir}/${p_dir}
     video_name=$(jq ".Title" *.info | tr -d "[:punct:]\040\011\012\015")
     part_name=$(jq ".PartName" *.info | tr -d "[:punct:]\040\011\012\015")
     upload_time=$(grep -Eo "20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]" *.info)
     Uploader=$(jq ".Uploader" *.info | tr -d "[:punct:]\040\011\012\015")
+    mp4_audio=$(jq ".VideoDashInfo" *.info | tr -d "[:punct:]\040\011\012\015")
+    
     if [ "null" = "${part_name}" ];then
       mp4_file_name=${video_name}.mp4
     else
       mp4_file_name=${video_name}_${p_dir}_${part_name}.mp4
     fi
-    ls *.flv | sort -n > ff.txt
-    sed -i 's/^/file /g' ff.txt
-    ffmpeg -f concat -i ff.txt -c copy ${mp4_dir}/${up_name}/"${mp4_file_name}";rm -rf ff.txt
-    cd ${root_dir}/${dir}
-  cd ${root_dir}
+
+    if [ "null" = "${mp4_audio}" ];then
+      ls *.flv | sort -n > ff.txt
+      sed -i 's/^/file /g' ff.txt
+      ffmpeg -f concat -i ff.txt -c copy ${mp4_dir}/${up_name}/"${mp4_file_name}";rm -rf ff.txt
+    else
+      ffmpeg  -i video.mp4 -i audio1.mp4 -c:v copy -c:a copy ${mp4_dir}/${up_name}/"${mp4_file_name}"
+    fi
+    cd ${download_dir}/${video_dir}
+  cd ${download_dir}
   done
 # 如果需要保留原视频请注释掉下面这一行
-# rm -rf ${root_dir}/${dir}
+#rm -rf ${download_dir}/${video_dir}
 done
